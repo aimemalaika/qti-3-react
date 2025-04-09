c667c253acce830de54d051b2be44420206cf743
Aime Malaika <aimemalaika1995@gmail.com>
2025-04-09 20:33:57 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { Player } from './Player';
+import * as cheerio from 'cheerio';
+
+export const App = () => {
+  const [items, setItems] = useState<{ href: string; identifier: string }[]>(
+    []
+  );
+
+  const assets = '/assets';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch(`${assets}/imsmanifest.xml`);
+      const xml = await data.text();
+
+      const $ = cheerio.load(xml);
+
+      const resources: { href: string; identifier: string }[] = [];
+
+      $('resource').each((_, element) => {
+        const identifier = $(element).attr('identifier')!;
+        const href = $(element).attr('href')!;
+
+        resources.push({ identifier, href });
+      });
+
+      setItems(resources);
+    };
+    fetchData().catch(console.error);
+  }, []);
+
+  return (
+    <>
+      <Player items={items} assets={assets}></Player>
+    </>
+  );
+};
